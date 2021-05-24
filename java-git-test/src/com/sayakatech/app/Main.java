a93307a9491aa030b41d6c5ca82dace79179af81
Jimmy Salazar <jimmyr.salazar@gmail.com>
2021-05-24 12:44:24 -0500

    First Commit

---
+++ java-git-test/src/com/sayakatech/app/Main.java
@@ -0,0 +1,11 @@
+package com.sayakatech.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola desde sayakatech!");
+		System.out.println("Probando EGit!!!");
+	}
+
+}
